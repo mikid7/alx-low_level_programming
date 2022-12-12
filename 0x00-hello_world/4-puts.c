c22e22ad37ac4aa71db86354db6ca2a4d1631565
mikid7 <kidistbeletu@gmail.com>
2022-12-12 06:31:50 -0800

    fix betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints a string
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
